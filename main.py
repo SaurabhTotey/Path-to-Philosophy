5ef1ad3f7256583569e2150deb3fe4c53b6f08aa
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-31 11:30:33 -0600

    Added basic way to get all subpages

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
 import sys
 
 startPage = sys.argv[1] if len(sys.argv) > 1 else "Python (programming language)"
-print(startPage)
+pageBranches = {}
+
+def getSubpages(page):
+    def asURL(page):
+        return "https://en.wikipedia.org/wiki/" + page.replace(" ", "_")
+    return [link.attrs["title"] for link in filter(lambda link: "title" in link.attrs, BeautifulSoup(requests.get(asURL(page)).text, "lxml").body.find("div", id="content").find("div", id="bodyContent").find("div", id="mw-content-text").find_all("a"))]
+
+
+print(getSubpages(startPage))
