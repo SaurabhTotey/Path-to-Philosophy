85773363a64e85d7c8cd2f2571fa0686ee2660bc
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-31 19:24:32 -0600

    Made program continue until Philosophy is found: is EXTREMELY slow

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ from bs4 import BeautifulSoup
 from functools import reduce
 import requests
 import sys
-
-startPage = sys.argv[1] if len(sys.argv) > 1 else "Python (programming language)"
-pageBranches = {}
+from threading import Thread
 
 
 def subpages_of(page):
+    """
+    Gets all the subpages or referenced pages from a given page name
+    :param page: the name of the page to find the subpages of
+    :return: a set of all the names of referenced pages
+    """
     def as_url(page_name):
         return "https://en.wikipedia.org/wiki/" + page_name.replace(" ", "_")
 
-    return set(link.attrs["title"] for link in filter(lambda link: "title" in link.attrs, reduce(lambda all_a, current_p: (all_a if type(all_a) is list else [all_a]) + list(current_p.find_all("a")), BeautifulSoup(requests.get(as_url(page)).text, "lxml").body.find("div", id="content").find("div", id="bodyContent").find("div", id="mw-content-text").find_all("p"))))
+    try:
+        return {link.attrs["title"] for link in filter(lambda link: "title" in link.attrs, reduce(lambda all_a, current_p: (all_a if type(all_a) is list else [all_a]) + list(current_p.find_all("a")), BeautifulSoup(requests.get(as_url(page)).text, "lxml").body.find("div", id="content").find("div", id="bodyContent").find("div", id="mw-content-text").find_all("p")))}
+    except:
+        return None
 
 
-print(subpages_of(startPage))
+startPage = sys.argv[1] if len(sys.argv) > 1 else "Python (programming language)"
+pageBranches = {startPage: subpages_of(startPage)}
+while "Philosophy" not in pageBranches:
+    for page in list(pageBranches):
+        for referenced in pageBranches[page]:
+            if referenced in pageBranches:
+                continue
+            def add_pages():
+                pageBranches[referenced] = subpages_of(referenced)
+            Thread(target=add_pages())
+    print(pageBranches)
